16089fcc5d49e6ff4831fb43c580cdb35756fe91
Svetlana <svetl.luckina2016@yandex.ru>
2024-01-09 18:33:45 +0300

    init commit hw

--- hw/main.py
+++ hw/main.py
@@ -2,6 +2,61 @@ from wine import Wine
 from beer import Beer
 from market import Market
 
+
+wine1 = Wine(title="Merlot", production_date="2022-01-01")
+wine2 = Wine(title="Chardonnay", production_date="2021-12-15")
+wine3 = Wine(title="Cabernet Sauvignon", production_date="2022-02-10")
+wine4 = Wine(title="Дербентское", production_date="2023-05-12")
+
+beer1 = Beer(title="Heineken", production_date="2022-03-05")
+beer2 = Beer(title="Guinness", production_date="2023-12-05")
+beer3 = Beer(title="Corona", production_date="2022-04-30")
+beer4 = Beer(title="Carlsberg", production_date="2023-09-16")
+
+
+market = Market(wines=[wine1, wine2, wine3, wine4], beers=[beer1, beer2, beer3, beer4])
+market2 = Market()
+
+
+print("\n*************** Проверка: в наличие ли Merlot в маркете. Должно быть True")
+print(market.has_drink_with_title("Merlot"))  # True
+print("\nПроверка: в наличие ли Балтика в маркете:")
+print(market.has_drink_with_title("Балтика"))  # False
+print("\nПроверка: в наличие ли None в маркете:")
+print(market.has_drink_with_title())  # False
+
+print("\n*************** Проверка: в наличие ли Merlot в маркете 2 (пустой):")
+print(market2.has_drink_with_title("Merlot"))
+
+print("\n*************** Сортировка напитков по названию:")
+sorted_drinks = market.get_drinks_sorted_by_title()
+i = 0
+for drink in sorted_drinks:
+    i += 1
+    print(str(i) + ") " + drink.title)
+
+print("\n*************** Получаем список напитков в диапазоне даты производства с 2022-01-01 до 2022-03-31:")
+filtered_drinks = market.get_drinks_by_production_date(from_date="2022-01-01", to_date="2022-03-31")
+i = 0
+for drink in filtered_drinks:
+    i += 1
+    print(str(i) + ") " + drink.title + ", дата изг. " + drink.production_date)
+
+print("\n*************** Получаем список напитков без указание диапозона дат производства:")
+filtered_drinks = market.get_drinks_by_production_date()
+i = 0
+for drink in filtered_drinks:
+    i += 1
+    print(str(i) + ") " + drink.title + ", дата изг. " + drink.production_date)
+
+
+print("\n*************** Сортировка напитков по названию в маркете 2 (пустой):")
+sorted_drinks = market2.get_drinks_sorted_by_title()
+i = 0
+for drink in sorted_drinks:
+    i += 1
+    print(str(i) + ") " + drink.title)
+
 """
 TODO: Доработать заготовки классов вина (Wine), пива (Beer) и магазина (Market) таким образом, чтобы через класс Market можно было:
 
--- hw/market.py
+++ hw/market.py
@@ -1,7 +1,24 @@
+import time
+
+
+def log_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = (end_time - start_time) * 10 ** 6 #мкс
+        formatted_start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
+        print(f"Старт метода {func.__name__} в {formatted_start_time}.")
+        print(f"Выполнение метода {func.__name__} завершилось. Время выполнения: {execution_time:.3f} мкс.")
+        return result
+    return wrapper
+
 class Market:
     def __init__(self, wines: list = None, beers: list = None) -> None:
-        pass
+        self.wines = {drink.title: drink for drink in wines} if wines is not None else {}
+        self.beers = {drink.title: drink for drink in beers} if beers is not None else {}
 
+    @log_time
     def has_drink_with_title(self, title=None) -> bool:
         """
         Проверяет наличие напитка в магазине за О(1)
@@ -9,20 +26,37 @@ class Market:
         :param title:
         :return: True|False
         """
-        pass
+        if title in self.wines or title in self.beers:
+            return True
+        else:
+            return False
 
+    @log_time
     def get_drinks_sorted_by_title(self) -> list:
         """
         Метод получения списка напитков (вина и пива) отсортированных по title
 
         :return: list
         """
-        pass
+        drinks = list(self.wines.values()) + list(self.beers.values())
+        sorted_drinks = sorted(drinks, key=lambda drink: drink.title)
+        return sorted_drinks
 
+    @log_time
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
         """
         Метод получения списка напитков в указанном диапазоне дат: с from_date по to_date
 
         :return: list
         """
-        pass
+        drinks = list(self.wines.values()) + list(self.beers.values())
+
+        filtered_drinks = list(filter(
+            lambda drink: (from_date is None or drink.production_date >= from_date) and
+                          (to_date is None or drink.production_date <= to_date),
+            drinks
+        ))
+        sorted_filtered_drinks = sorted(filtered_drinks, key=lambda drink: drink.production_date)
+
+        return sorted_filtered_drinks
+
